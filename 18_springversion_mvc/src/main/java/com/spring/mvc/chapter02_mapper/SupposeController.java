c4c83c1602cd7f7f5a17d73e9384fe6ce5f6939c
12_web_hjs <12_web_hjs@DESKTOP-2RF42K3>
2023-05-18 14:40:31 +0900

    커밋

--- 18_springversion_mvc/src/main/java/com/spring/mvc/chapter02_mapper/SupposeController.java
+++ 18_springversion_mvc/src/main/java/com/spring/mvc/chapter02_mapper/SupposeController.java
@@ -11,6 +11,9 @@ public class SupposeController {
 	@Autowired
 	private M2D m2d;
 	
+	@Autowired
+	private D2M d2m;
+	
 	
 /*  Mapper to DAO */
 	
@@ -55,6 +58,89 @@ public class SupposeController {
 		m2d.ex07();
 		return "home";
 	}
+	
+	
+	@GetMapping(value="/ex08")
+	public String ex08() {
+		m2d.ex08();
+		return "home";
+	}
+	
+	@GetMapping(value="/ex09")
+	public String ex09() {
+		m2d.ex09();
+		return "home";
+	}
+	
+	@GetMapping(value="/ex10")
+	public String ex10() {
+		m2d.ex10();
+		return "home";
+	}
+	
+	@GetMapping(value="/ex11")
+	public String ex11() {
+		m2d.ex11();
+		return "home";
+	}
+	
+	
+/*  DAO to Mapper */
+	
+	@GetMapping(value="/sample01")
+	public String sample01() {
+		d2m.sample01();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample02")
+	public String sample02() {
+		d2m.sample02();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample03")
+	public String sample03() {
+		d2m.sample03();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample04")
+	public String sample04() {
+		d2m.sample04();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample05")
+	public String sample05() {
+		d2m.sample05();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample06")
+	public String sample06() {
+		d2m.sample06();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample07")
+	public String sample07() {
+		d2m.sample07();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample08")
+	public String sample08() {
+		d2m.sample08();
+		return "home";
+	}
+	
+	@GetMapping(value="/sample09")
+	public String sample09() {
+		d2m.sample09();
+		return "home";
+	}
+	
 	
 	
 }
