6f801a63402b82f4cbe6778254895a12b0bbcc72
12_web_hjs <12_web_hjs@DESKTOP-2RF42K3>
2023-05-26 14:39:48 +0900

    최초커밋

--- 18_springversion_mvc/src/main/java/com/spring/mvc/chapter03_dynamicQuery/DynamicQueryDAO.java
+++ 18_springversion_mvc/src/main/java/com/spring/mvc/chapter03_dynamicQuery/DynamicQueryDAO.java
@@ -1,5 +1,7 @@
 package com.spring.mvc.chapter03_dynamicQuery;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.spring.mvc.dto.BrandDTO;
 import com.spring.mvc.dto.ProductDTO;
 
 @Repository
@@ -60,14 +63,35 @@ public class DynamicQueryDAO {
 		
 		System.out.println("\n --- otherwiseEx --- \n");
 		
+		int deliveryPrice = 0;
+		//int deliveryPrice = 3000;
 		
+		List<ProductDTO> productList = sqlSession.selectList("dynamicQuery.otherwiseEx", deliveryPrice);
+		for (ProductDTO productDTO : productList) {
+			System.out.println(productDTO);
+		}
 	}
 	
 	// foreach 사용 예시1
 	public void foreachEx01() {
 		
 		System.out.println("\n --- foreachEx01 --- \n");
+		System.out.println("\n --- foreachEx01 --- \n");
 		
+		List<BrandDTO> brandList = new ArrayList<BrandDTO>();
+		
+		for (int i = 105; i < 110; i++) {
+			
+			BrandDTO brandDTO = new BrandDTO();
+			brandDTO.setBrandCd("b" + i);
+			brandDTO.setBrandNm("추가된브랜드" + i);
+			brandDTO.setEnteredDt(new Date());
+			brandDTO.setActiveYn("Y");
+			
+			brandList.add(brandDTO);
+		}
+		
+		sqlSession.insert("dynamicQuery.foreachEx01" , brandList);
 	}
 	
 	
@@ -75,7 +99,13 @@ public class DynamicQueryDAO {
 	public void foreachEx02() {
 		
 		System.out.println("\n --- foreachEx02 --- \n");
-	
+		
+		
+		String[] brandCdList = {"b1" , "b2" , "b3"};
+		List<BrandDTO> brandList = sqlSession.selectList("dynamicQuery.foreachEx02" ,  brandCdList);
+		for (BrandDTO brandDTO : brandList) {
+			System.out.println(brandDTO);
+		}
 	}
 	
 	
@@ -83,7 +113,8 @@ public class DynamicQueryDAO {
 	public void foreachEx03() {
 		
 		System.out.println("\n --- foreachEx03 --- \n");
-		
+		String[] brandCdList = {"b100" , "b101" , "b102" , "b103" , "b104" };
+		sqlSession.delete("dynamicQuery.foreachEx03" ,  brandCdList);
 	}
 	
 	
@@ -91,7 +122,8 @@ public class DynamicQueryDAO {
 	public void foreachEx04() {
 		
 		System.out.println("\n --- foreachEx04 --- \n");
-		
+		int[] productCdList = {1,2,3,4,5,6};
+		sqlSession.update("dynamicQuery.foreachEx04" ,  productCdList);
 	}
 	
 	
@@ -99,6 +131,17 @@ public class DynamicQueryDAO {
 	public void whereEx() {
 		
 		System.out.println("\n --- whereEx --- \n");
+		ProductDTO productDTO = new ProductDTO();
+		// 1) productNm과 brandCd가 모두 있을 경우 > 정상
+		// 2) productNm만 있을 경우 > 정상
+		// 3) brandCd만 있을 경우 > 오류
+		productDTO.setProductNm("삼성");
+		productDTO.setBrandCd("b1");
+		
+		List<ProductDTO> productList = sqlSession.selectList("dynamicQuery.whereEx" ,  productDTO);
+		for (ProductDTO dto : productList) {
+			System.out.println(dto);
+		}
 		
 	}
 	
@@ -107,7 +150,15 @@ public class DynamicQueryDAO {
 	public void setEx() {
 		
 		System.out.println("\n --- setEx --- \n");
+		ProductDTO productDTO = new ProductDTO();
+		// 1) price와 deliveryPrice가 모두 있을 경우 > 정상
+		// 2) price만 있을 경우 > 오류
+		// 3) deliveryPrice만 있을 경우 > 정상
+		productDTO.setPrice(1);
 		
+		//productDTO.setDeliveryPrice(1);
+		
+		sqlSession.update("dynamicQuery.setEx" ,  productDTO);
 	}
 	
 	
